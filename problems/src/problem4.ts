3a2a6c891c12a5276dcaee528832e0b02a96ffbc
MadihaJarrin <madihajarrin1@gmail.com>
2024-11-08 18:05:23 +0600

    up to problem 5 done

---
+++ problems/src/problem4.ts
@@ -0,0 +1,35 @@
+type Circle = {
+    shape: "circle",//literal type
+    radius: number
+};
+type Rectangle = {
+    shape: "rectangle", //literal type
+    height: number,
+    width: number
+};
+
+type shape = Circle | Rectangle; //define union type 
+
+//to calculate shape Area 
+function calculateShapeArea(shape: shape): number {
+    if (shape.shape === "circle") {
+        return parseFloat((Math.PI * shape.radius ** 2).toFixed(2)); //rounded to 2 decimal places.
+    } else if (shape.shape === "rectangle") {
+        return shape.height * shape.width;
+    } else {
+        return 0;
+    }
+}
+
+// Sample Input 1:
+const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+console.log(circleArea); // Sample Output 1: 78.54;
+
+// Sample Input 2:
+const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 4,
+    height: 6,
+});
+
+console.log(rectangleArea) // Sample Output 2: 24;
