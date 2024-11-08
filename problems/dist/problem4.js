8e17d22368ac4f85474b45e2ef8b8dbb91c1bfdf
MadihaJarrin <madihajarrin1@gmail.com>
2024-11-08 18:57:52 +0600

    problem 5 done

--- problems/dist/problem4.js
+++ problems/dist/problem4.js
@@ -1,1 +1,23 @@
 "use strict";
+//to calculate shape Area 
+function calculateShapeArea(shape) {
+    if (shape.shape === "circle") {
+        return parseFloat((Math.PI * shape.radius ** 2).toFixed(2)); //rounded to 2 decimal places.
+    }
+    else if (shape.shape === "rectangle") {
+        return shape.height * shape.width;
+    }
+    else {
+        return 0;
+    }
+}
+// Sample Input 1:
+const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+console.log(circleArea); // Sample Output 1: 78.54;
+// Sample Input 2:
+const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 4,
+    height: 6,
+});
+console.log(rectangleArea); // Sample Output 2: 24;
---
+++ problems/src/problem5.ts
@@ -0,0 +1,17 @@
+type T = {
+    name: string,
+    age: number
+}
+
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+}
+
+
+// Sample Input:
+const person = {
+    name: "Alice",
+    age: 30
+};
+
+console.log(getProperty(person, "name")); // Sample Output: Alice;
